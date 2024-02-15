4ac4b622c4f6236fc7712e434a6bed07ff405259
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2024-02-15 07:23:14 -0300

    init

---
+++ app.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+useHead({
+  title: 'Repository Pattern'
+})
+
+const { data: posts }: any = await useAsyncData(() => $fetch('https://jsonplaceholder.typicode.com/posts'))
+</script>
+
+<template>
+  <div class="mx-auto p-8 flex items-center justify-center">
+    <ul>
+      <span class="text-xl font-medium mb-2 block">Posts:</span>
+      <li v-for="post in posts" :key="post.id" class="mb-2">
+        {{ post.title }}
+      </li>
+    </ul>
+  </div>
+</template>
